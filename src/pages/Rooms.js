348f6cc4626b19cbfd7a0bc4f0095841d16db122
karan <karanraj17111996@gmail.com>
2020-09-24 22:37:22 +0530

    added rooms page and functionality

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Banner from '../components/Banner'
 import Hero from '../components/Hero'
 import { Link } from "react-router-dom";
+import RoomsContainer from "../components/RoomsContainer";
 
 function Rooms() {
     return (
-        <Hero hero="roomsHero">
-            <Banner title="our rooms" subtitle=""><Link to="/" className="btn-primary">return home</Link></Banner>
-        </Hero>
+        <>
+            <Hero hero="roomsHero">
+                <Banner title="our rooms" subtitle=""><Link to="/" className="btn-primary">return home</Link></Banner>
+            </Hero>
+            <RoomsContainer />
+        </>
     )
 }
 
