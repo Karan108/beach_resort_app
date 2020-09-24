ff8eb61ce14c39a3c50d7a680291e092b6b46f65
karan <karanraj17111996@gmail.com>
2020-09-24 16:05:54 +0530

    basic pages created

---
+++ src/pages/Rooms.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Hero from '../components/Hero'
+import { Link } from "react-router-dom";
+
+function Rooms() {
+    return (
+        <Hero hero="roomsHero">
+            <Banner title="our rooms" subtitle=""><Link to="/" className="btn-primary">return home</Link></Banner>
+        </Hero>
+    )
+}
+
+export default Rooms
